45b8cfda97fd7ed742d2a678ac15dd73542ba986
roushk <34784335+roushk@users.noreply.github.com>
2021-03-04 21:25:25 -0800

    updated to new shader pipeline

---
+++ 2DPlatformer/Assets/Scripts/SmoothCamera.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour
+{
+
+    public Transform target;
+    public Prime31.CharacterController2D targetRigid;
+    public PlayerMovement playerMove;
+
+    public float smoothingSpeed = 5f;
+    public float MovementVectorSmoothing = 0.4f;
+    public Vector3 offset = new Vector3(0, 0.5f, -1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //camera = this.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 betterOffset = offset + new Vector3(targetRigid.velocity.x * MovementVectorSmoothing, targetRigid.velocity.y * MovementVectorSmoothing * 0.8f, -1);
+
+        transform.position = Vector3.Lerp(transform.position, target.position + betterOffset, smoothingSpeed * Time.deltaTime);
+    }
+}
